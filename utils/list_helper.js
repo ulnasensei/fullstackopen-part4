7e648cedef1c290a49206065401eb795272644ca
Ulna <30731799+ulnasensei@users.noreply.github.com>
2023-05-22 19:42:29 +0300

    4.5 to 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1;
@@ -8,7 +10,48 @@ const totalLikes = (blogs) => {
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((prev, current) => {
+    if (current.likes > prev.likes) return current;
+    else return prev;
+  });
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
+const mostBlogs = (blogs) => {
+  const groupedByAuthor = _.groupBy(blogs, 'author');
+  const authorsAndNumbers = Object.keys(groupedByAuthor).map((key) => ({
+    author: key,
+    blogs: groupedByAuthor[key].length,
+  }));
+
+  return authorsAndNumbers.reduce((prev, current) => {
+    if (current.blogs > prev.blogs) return current;
+    else return prev;
+  });
+};
+
+const mostLikes = (blogs) => {
+  const groupedByAuthor = _.groupBy(blogs, 'author');
+  const authorsAndTotalLikes = Object.keys(groupedByAuthor).map((key) => {
+    const totalLikes = groupedByAuthor[key].reduce((total, blog) => total + blog.likes, 0);
+    return { author: key, likes: totalLikes };
+  });
+
+  return authorsAndTotalLikes.reduce((prev, current) => {
+    if (current.likes > prev.likes) return current;
+    else return prev;
+  });
+};
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 };
