427960b2ebd16dbba76aafd855f3a2c9528e8d0a
Ulna <30731799+ulnasensei@users.noreply.github.com>
2023-05-22 18:24:23 +0300

    4.2 Blog list, step2

---
+++ utils/middleware.js
@@ -0,0 +1,31 @@
+const logger = require('./logger');
+const morgan = require('morgan');
+
+morgan.token('body', function (req) {
+  return JSON.stringify(req.body);
+});
+
+const logFormat = ':method :url :status :res[content-length] - :response-time ms :body';
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = {
+  requestLogger: morgan,
+  logFormat,
+  unknownEndpoint,
+  errorHandler
+};
