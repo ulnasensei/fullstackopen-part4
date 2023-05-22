db6530c5f80e3fb86d2565f15bee278e3052d942
Ulna <30731799+ulnasensei@users.noreply.github.com>
2023-05-22 18:51:30 +0300

    4.3 and 4.4: helper functions and unit tests

---
+++ utils/list_helper.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  return total;
+};
+
+module.exports = {
+  dummy,
+  totalLikes
+};
