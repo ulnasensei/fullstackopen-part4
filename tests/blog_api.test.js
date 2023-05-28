fb8c30de7d08a245e33b25f619322044e948dc62
Ulna <30731799+ulnasensei@users.noreply.github.com>
2023-05-28 23:57:33 +0300

    fixed 4.10 and 4.11

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -7,11 +7,11 @@ const api = supertest(app);
 
 const Blog = require('../models/blog');
 
-beforeEach(async() => {
+beforeEach(async () => {
   await Blog.deleteMany({});
 
-  const blogObjects = helper.initialBlogs.map(blog => Blog(blog));
-  const promiseArray = blogObjects.map(blog => blog.save());
+  const blogObjects = helper.initialBlogs.map((blog) => Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
   await Promise.all(promiseArray);
 });
 describe('blogs get', () => {
@@ -21,34 +21,48 @@ describe('blogs get', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/);
   });
-  test('expect 2 blogs to be returned', async() => {
+  test('expect 2 blogs to be returned', async () => {
     const response = await api.get('/api/blogs ');
 
     expect(response.body).toHaveLength(helper.initialBlogs.length);
   });
-  test('unique identifier property is named id', async() => {
+  test('unique identifier property is named id', async () => {
     const response = await api.get('/api/blogs');
 
     expect(response.body[0].id).toBeDefined();
   });
-
 });
 
 describe('blogs post', () => {
-  test('blog saved successfully', async() => {
-    const blog = new Blog({ title: 'testing post', author: 'test author', url: 'test url', likes: 1 });
-    await blog.save();
+  test('blog saved successfully', async () => {
+    await api
+      .post('/api/blogs')
+      .send({
+        title: 'testing post',
+        author: 'test author',
+        url: 'test url',
+        likes: 1,
+      })
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
     const response = await api.get('/api/blogs ');
 
     expect(response.body).toHaveLength(helper.initialBlogs.length + 1);
   });
 
-  test('likes set to 0 if missing from request', async() => {
-    const blog = new Blog({ title: 'testing post', author: 'test author', url: 'test url' });
-    const savedBlog = await blog.save();
+  test('likes set to 0 if missing from request', async () => {
+    const savedBlog = await api
+      .post('/api/blogs')
+      .send({
+        title: 'testing post with no likes',
+        author: 'test author 2',
+        url: 'test url 2',
+      })
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-    expect(savedBlog.likes).toEqual(0);
+    expect(savedBlog.body.likes).toEqual(0);
   });
 });
 
